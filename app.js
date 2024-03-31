d5216dca316640367ea792dc406dddc442d1059f
Athazaaa <165560435+Athazaaa@users.noreply.github.com>
2024-03-31 09:00:00 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,12 @@
-fetch('sites.json')
-    .then(response => response.json())
-    .then(sites => {
-        const linksContainer = document.getElementById('links-container');
+$(document).ready(function() {
+    $.getJSON('sites.json', function(data) {
+        var sites = data.sites;
+        var container = $('.container');
 
-        sites.forEach(site => {
-            const { name, link, image } = site;
-
-            const linkItem = document.createElement('li');
-            const linkAnchor = document.createElement('a');
-            linkAnchor.href = link;
-            linkAnchor.textContent = name;
-
-            const faviconUrl = `/images/${image}`;
-            const img = new Image();
-            img.src = faviconUrl;
-
-            img.onload = function() {
-                linkAnchor.style.backgroundImage = `url(${img.src})`;
-            };
-
-            img.onerror = function() {
-                linkAnchor.style.backgroundImage = 'url(red-x.png)'; // Fallback to red X image
-                linkAnchor.style.backgroundSize = 'contain';
-                linkAnchor.style.backgroundRepeat = 'no-repeat';
-                linkAnchor.style.backgroundPosition = 'center';
-            };
-
-            linkItem.appendChild(linkAnchor);
-            linksContainer.appendChild(linkItem);
+        sites.forEach(function(site) {
+            var link = $('<div class="link"><img src="' + site.image + '" alt="' + site.name + '"><p>' + site.name + '</p></div>');
+            link.appendTo(container);
+            link.wrap('<a href="' + site.link + '" target="_blank"></a>');
         });
-    })
-    .catch(error => {
-        console.error('Error loading sites:', error);
     });
+});
